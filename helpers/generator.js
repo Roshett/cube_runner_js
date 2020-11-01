2903b73c601fa4370ac7da9841ae48956a8ef7be
ValeryHizhevsky <roshette@yandex.ru>
2020-11-01 17:39:04 +0300

    update level

--- helpers/generator.js
+++ helpers/generator.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const data = JSON.parse(fs.readFileSync("testdata.json", "utf8"));
 
 let level = [];
-const EMPTY = true;
+const EMPTY = false;
 
 for (let index = 0; index < 1000; index++) {
   if (EMPTY) {
